bbbfb3b5d381ac50295441adc522cf5f1909f7e4
dalarcon80 <63214788+dalarcon80@users.noreply.github.com>
2023-10-02 13:19:03 -0500

    Update App.js

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,195 +1,56 @@
-import '../header.css'
-import Navbar from '../Components/Navbar'
-import axios from "axios";
+import React, { useEffect, useRef } from 'react';
 
-import '../App.css'
-import { useState } from 'react'
-import React from 'react';
-//import ChatMessage from './Components/ChatMessage';
-//import Loader from './Components/Loader';
-import Aside from '../Components/AsideComponent';
-import ReciclerViewChat from '../Components/ReciclerViewChat';
-//import ChatInputHolder from './Components/ChatInputTextHolderComponent';
-//import { BrowserRouter, Routes, Route } from 'react-router-dom'
+function App() {
+  // Ref para la etiqueta de vídeo
+  const videoRef = useRef(null);
 
+  useEffect(() => {
+    // Código para cargar el script de Soul Machines
+    const script = document.createElement('script');
+    script.src = 'https://static.soulmachines.com/widget-snippet-1.12.0.min.js';
+    script.dataset.smApiKey = 'YOUR_API_KEY'; // Reemplaza con tu clave API
 
-const url = "https://am-ecp-llm.azure-api.net/func-openai-llm-ecp/HttpTrigger1_API"
-const urlUploadFile = "https://sallmpocecpsbx.blob.core.windows.net/bronze"
+    // Añadir el script al cuerpo del documento
+    document.body.appendChild(script);
 
-const apiKeyUploadBlobStorage = "UNOHOEG5HF1G4cZTgiBrU+4HHJhsxRW0uA/faIiiirHueJKf3nK5Zl2JDlaeyUmH+nM3d1f5Qvek+ASt68fi0Q=="
+    // Código para acceder a la cámara web y mostrarla en la mitad derecha
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        videoRef.current.srcObject = stream;
+      })
+      .catch((error) => {
+        console.error('Error al acceder a la cámara: ', error);
+      });
 
-const fetchData = async (query) => {
-    const payload = {
-        query: query + ". Responde en español y especifica el source."
+    return () => {
+      // Limpia el script cuando el componente se desmonte
+      document.body.removeChild(script);
     };
+  }, []);
 
-    try {
-        console.log("\nCargando respuesta ...\n");
-
-        const response = await axios.post(url, payload);
-        console.log("respuesta ", response)
-
-        if (response.status === 200) {
-            const data = response.data;
-            //console.log('Respuesta:', data.response);
-            return data.response;
-        } else {
-            console.error('Error:', response.data);
-            throw new Error('Something went wrong');
-        }
-    } catch (error) {
-        console.error('Error:', error.message);
-        throw new Error('Something went wrong');
-    }
-};
-
-const App = () => {
-
-    const [input, setInput] = useState("")
-    const [loading, setLoading] = useState(false);
-    const [loadFile, setLoadFile] = useState(null);
-    const [progress, setProgress] = useState({ started: false, pc: 0 });
-    const fileInputRef = React.createRef();
-
-
-    const [chatlog, setChatLog] = useState([{
-        user: "genaichat",
-        message: "Hola soy GenAI de Accenture te ayudare con todas tus consultas. " +
-            "Tengo limitaciones y a veces me equivoco, pero tus " +
-            "comentarios me ayudarán a mejorar.",
-        loadingMsn: 1
-    }
-    ])
-
-    function handleSubmit(e) {
-        e.preventDefault();
-
-        setLoading(true)
-
-
-        setChatLog([...chatlog, { user: "genaichat", message: `${input}`, loadingMsn: 2 }])
-        setInput("")
-
-        async function response() {
-            try {
-                const response = await fetchData(input);
-                //apago animacion
-                setChatLog(chatlog => [...chatlog, { user: "genAI", message: `${response}`, loadingMsn: 3 }])
-                setLoading(false)
-
-            } catch (error) {
-                setLoading(false)
-                console.error('Error:', error.message);
-                setChatLog([...chatlog, { user: "genAI", message: "Respuesta no encontrada..." }])
-            }
-
-        }
-
-        response()
-
-
-    };
-
-    const handleFileSelected = () => {
-        fileInputRef.current.click();
-    }
-
-
-
-
-    const handleFileUpload = (file) => {
-        if (!file) {
-            setChatLog(chatlog => [...chatlog, {
-                user: "genaichat",
-                message: "No se ha seleccionado ningún archivo",
-                loadingMsn: 3
-            }])
-
-            return;
-        }
-
-        setChatLog(chatlog => [...chatlog, {
-            user: "genaichat",
-            message: `Subiendo el archivo: ${file.name}`,
-            loadingMsn: 3
-        }])
-
-        const formData = new FormData();
-        formData.append('file', file);
-
-        axios.put(`${urlUploadFile}?${apiKeyUploadBlobStorage}`, formData, {
-            headers: {
-                'x-ms-blob-type': 'BlockBlob',
-                'Content-Type': 'multipart/form-data'
-            },
-        })
-            .then((response) => {
-                // Handle successful upload response
-                console.log('File uploaded successfully:', response.data);
-            })
-            .catch((error) => {
-                // Handle upload error
-                console.error('Error uploading file:', error.response.data);
-            });
-
-    }
-
-    const handleFileChange = (event) => {
-        setLoadFile(event.target.files[0]);
-        let file = event.target.files[0]
-        handleFileUpload(file)
-        console.log(file.name)
-
-        //handleFileUpload()
-
-
-
-    };
-
-
-
-    function clearChat() {
-        setChatLog([{
-            user: "genaichat",
-            message: "Hola soy GenAI de Accenture te ayudare con todas tus consultas. " +
-                "Tengo limitaciones y a veces me equivoco, pero tus " +
-                "comentarios me ayudarán a mejorar.",
-            loadingMsn: 1
-        }])
-
-    }
-
-
-    return (
-
-        <div className='App'>
-            <Navbar />
-            <div className='App-body'>
-                <Aside clearChat={clearChat} />
-                <section className='chatbox'>
-                    <ReciclerViewChat loading={loading} chatlog={chatlog} />
-                    <div
-                        className='chat-input-textholder'>
-                        <form onSubmit={handleSubmit}>
-
-                            <input
-                                rows={1}
-                                value={input}
-                                onChange={(e) => setInput(e.target.value)}
-                                className='chat-input-textarea'
-                                placeholder='Escribe tu pregunta ...'>
-                            </input>
-
-                        </form>
-                    </div>
-                </section>
-            </div>
-        </div>
-
-
-
-
-    )
-
+  return (
+    <div className="App">
+      <div className="left-half">
+        {/* Contenedor para el script de Soul Machines */}
+        {/* Asegúrate de reemplazar YOUR_API_KEY con tu clave API real */}
+        <script
+          src="https://static.soulmachines.com/widget-snippet-1.12.0.min.js"
+          data-sm-api-key="eyJzb3VsSWQiOiJkZG5hLWRhbmllbC1hbGFyY29uLS1kaWdpdGFsLWNoaWVmLWV4cGVyaWVuY2UiLCJhdXRoU2VydmVyIjoiaHR0cHM6Ly9kaC5hei5zb3VsbWFjaGluZXMuY2xvdWQvYXBpL2p3dCIsImF1dGhUb2tlbiI6ImFwaWtleV92MV9lNmYxZTA3OS00ODI1LTQ2YjUtYjE5ZC05YjEwYzRjNDUzNjEifQ=="
+        ></script>
+      </div>
+      <div className="right-half">
+        {/* Contenedor para mostrar la cámara web */}
+        <video
+          ref={videoRef}
+          autoPlay
+          playsInline
+          width="100%"
+          height="100%"
+        ></video>
+      </div>
+    </div>
+  );
 }
+
 export default App;
