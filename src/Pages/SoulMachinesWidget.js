8a626b635db16d8d228068cdf45a68502ba0e0e6
dalarcon80 <63214788+dalarcon80@users.noreply.github.com>
2023-10-02 13:36:15 -0500

    Create SoulMachinesWidget.js

---
+++ src/Pages/SoulMachinesWidget.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SoulMachinesWidget = () => {
+  return (
+    <div>
+      <script
+        src="https://static.soulmachines.com/widget-snippet-1.12.0.min.js"
+        data-sm-api-key="eyJzb3VsSWQiOiJkZG5hLWRhbmllbC1hbGFyY29uLS1kaWdpdGFsLWNoaWVmLWV4cGVyaWVuY2UiLCJhdXRoU2VydmVyIjoiaHR0cHM6Ly9kaC5hei5zb3VsbWFjaGluZXMuY2xvdWQvYXBpL2p3dCIsImF1dGhUb2tlbiI6ImFwaWtleV92MV9lNmYxZTA3OS00ODI1LTQ2YjUtYjE5ZC05YjEwYzRjNDUzNjEifQ=="
+      />
+    </div>
+  );
+};
+
+export default SoulMachinesWidget;
